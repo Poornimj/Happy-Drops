78b2702750c16413c622ec2e94fe8f917bf484cf
Poornimj <poornimj@metropolia.fi>
2026-06-03 00:52:57 +0300

    Added project assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
+      <Navbar />
+
       <h1>Happy Drops</h1>
       <p>Your website is starting successfully 🚀</p>
+      <p>Hurray ! </p>
     </div>
   );
 }
