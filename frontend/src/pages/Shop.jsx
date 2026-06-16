f722a2bb207472e0b330c61e86546867e1098ce6
HChathurika <hathadus@metropolia.fi>
2026-06-16 18:46:16 +0300

    Added Hover Images

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../shared.css"
 import "./Shop.css";
@@ -23,23 +24,41 @@ import SkinMoisture from "../assets/images/SkinMoistures.png";
 import AntiWrincle from "../assets/images/AntiWrincles.png";
 import StressRelief from "../assets/images/StressReliefs.png";
 
+// import Hover images
+
+import SkinCareHover from "../assets/images/SkinCaresHover.png";
+import immunityOilHover from "../assets/images/ImmunityOilHover.png";
+import painReliefOilHover from "../assets/images/JointEassesHover.png";
+import sleepOilHover from "../assets/images/TwilightDropssHover.png";
+import sleepSupportOilHover from "../assets/images/CherishmoodsHover.png";
+import stressOilHover from "../assets/images/HeadacheReliefsHover.png";
+import hairOilHover from "../assets/images/BoostHairGrowthsHover.png";
+import SleepSupOilHover from "../assets/images/SleepSupOilHover.jpg";
+import SkinTightOilHover from "../assets/images/SkinTightningsHover.png";
+import SkinMoistureHover from "../assets/images/SkinMoisturesHover.png";
+import AntiWrincleHover from "../assets/images/AntiWrinclesHover.png";
+import StressReliefHover from "../assets/images/StressReliefsHover.png";
+import healthyhairHover from "../assets/images/healthyhairHover.png";
+
+
 const products = [
-  { id: 1, name: "Skin and Beauty", desc: "Radiant skin care", price: "24.90", badge: "Best Seller", reviews: 120 ,image: SkinCare },
-  { id: 2, name: "Sleep and Relaxation", desc: "Calm mind and reduce stress", price: "29.90", badge: "Popular", reviews: 98, image: sleepOil},
-  { id: 3, name: "Hair Care", desc: "Nourishing hair care", price: "26.90", reviews: 76, image: hairOil},
-  { id: 4, name: "Sleep Support Blend", desc: "Relaxation and better sleep", price: "23.00", reviews: 120, image: sleepSupportOil },
-  { id: 5, name: "Pain and relief", desc: "Soothing body relief", price: "22.00", reviews: 120, image: painReliefOil },
-  { id: 6, name: "Boost Immunity", desc: "Immune defense support", price: "19.90", reviews: 120, image:immunityOil},
-  { id: 7, name: "Stress and Mood", desc: "Calm mood balance", price: "21.90", reviews: 120, image: stressOil },
-  { id: 8, name: "Skin Tightening", desc: "Firm and youthful skin", price: "27.90", reviews: 120, image: SkinTightOil },
-  { id: 9, name: "Skin and Beauty", desc: "Soft skin glow", price: "22.90", reviews: 120, image: SkinMoisture },
-  { id: 10, name: "Anti Wrinkle Oil", desc: "Smooths skin", price: "25.90", reviews: 98, image: AntiWrincle },
-  { id: 11, name: "Hair Strength Oil", desc: "Strong healthy hair", price: "27.90", reviews: 76, image: hairOil },
-  { id: 12, name: "Mood Harmony", desc: "Calm positivity", price: "24.90", reviews: 120, image: StressRelief },
+  { id: 1, name: "Skin and Beauty", desc: "Radiant skin care", price: "24.90", badge: "Best Seller", reviews: 120 ,image: SkinCare ,hoverImage: SkinCareHover },
+  { id: 2, name: "Sleep and Relaxation", desc: "Calm mind and reduce stress", price: "29.90", badge: "Popular", reviews: 98, image: sleepOil , hoverImage: sleepOilHover },
+  { id: 3, name: "Hair Care", desc: "Nourishing hair care", price: "26.90", reviews: 76, image: hairOil , hoverImage: hairOilHover },
+  { id: 4, name: "Sleep Support", desc: "Deep restful sleep", price: "27.90", reviews: 120, image: sleepSupportOil , hoverImage: sleepSupportOilHover },
+  { id: 5, name: "Pain and relief", desc: "Soothing body relief", price: "22.00", reviews: 120, image: painReliefOil , hoverImage: painReliefOilHover },
+  { id: 6, name: "Boost Immunity", desc: "Immune defense support", price: "19.90", reviews: 120, image:immunityOil , hoverImage: immunityOilHover },
+  { id: 7, name: "Stress and Mood", desc: "Calm mood balance", price: "21.90", reviews: 120, image: stressOil , hoverImage: stressOilHover },
+  { id: 8, name: "Skin Tightening", desc: "Firm and youthful skin", price: "27.90", reviews: 120, image: SkinTightOil , hoverImage: SkinTightOilHover },
+  { id: 9, name: "Skin and Beauty", desc: "Soft skin glow", price: "22.90", reviews: 120, image: SkinMoisture , hoverImage: SkinMoistureHover },
+  { id: 10, name: "Anti Wrinkle Oil", desc: "Smooths skin", price: "25.90", reviews: 98, image: AntiWrincle , hoverImage: AntiWrincleHover },
+  { id: 11, name: "Hair Strength Oil", desc: "Strong healthy hair", price: "27.90", reviews: 76, image: hairOil , hoverImage: healthyhairHover },
+  { id: 12, name: "Mood Harmony", desc: "Calm positivity", price: "24.90", reviews: 120, image: StressRelief , hoverImage: StressReliefHover },
 
 ];
 
 function Shop() {
+  const [hoveredProductId, setHoveredProductId] = useState(null);
   
 
   const addToCart = (event, product) => {
@@ -107,7 +126,16 @@ function Shop() {
                 </span>
               )}
 
-              <img src={product.image} alt={product.name} />
+              <img
+                 src={
+                   hoveredProductId === product.id && product.hoverImage
+                     ? product.hoverImage
+                     : product.image
+                }
+                alt={product.name}
+                onMouseEnter={() => setHoveredProductId(product.id)}
+                onMouseLeave={() => setHoveredProductId(null)}
+             />
 
               <div className="product-info">
                 <h3>{product.name}</h3>
